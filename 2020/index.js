bb08ef3d0f93a056fff284f560272d690eb84cdf
cocreativeworlds <71390843+cocreativeworlds@users.noreply.github.com>
2020-09-17 13:37:26 +0100

    Adds initial test ‘2020/index.js’

---
+++ 2020/index.js
@@ -0,0 +1,12 @@
+// 2020/index.js
+// Adds a list of all posts made in 2020 to the global POSTS array.
+
+function init() {
+  if (typeof window !== "object") return;
+  window.POSTS = window.POSTS || [];
+  window.POSTS = window.POSTS.concat([
+    "20200914-shortread-test-foo--title-in-here",
+    "20200917-longread-test-bar--another-title-in-here"
+  ]);
+}
+init();
